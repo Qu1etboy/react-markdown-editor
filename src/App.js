3dca2067d993bf3490431eb699745c583e66359c
Qu1etboy <nonzagreanthai@gmail.com>
2022-12-27 21:14:11 +0700

    Change markdown-it to react-markdown

--- src/App.js
+++ src/App.js
@@ -1,32 +1,52 @@
-import { useState, useEffect } from 'react';
-import md from 'markdown-it';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import Editor from "@monaco-editor/react";
+import remarkGfm from "remark-gfm";
+import rehypeRaw from "rehype-raw";
 
 function App() {
   const [value, setValue] = useState(() => {
-    const saveValue = window.localStorage.getItem('value');
-    console.log(saveValue);
+    const saveValue = window.localStorage.getItem("value");
+
     if (saveValue !== null) {
       return saveValue;
-    }
-    else {
-      return '';
+    } else {
+      return "";
     }
   });
 
   useEffect(() => {
-    window.localStorage.setItem('value', value);
+    window.localStorage.setItem("value", value);
   }, [value]);
 
-
   return (
-    <div className="flex flex-col justify-center items-center">
-      <h1 className='text-3xl font-bold m-5 text-center'>Markdown Editor</h1>
-      <div className='flex gap-5'>
-        <textarea className='border-2 border-neutral-700 rounded-md p-5'cols='60' rows='30' value={value} onInput={(e) => setValue(e.target.value)} placeholder='start by typing here...'></textarea>
-        <div className='w-[800px] h-[1000px] border-2 border-neutral-700 rounded-md p-5 prose prose-sm overflow-auto' dangerouslySetInnerHTML={{ __html: md().render(value) }}></div>
+    <div className="min-h-screen">
+      <nav className="flex justify-between items-center w-full p-5">
+        <h1 className="text-3xl font-bold text-center">Markdown Editor</h1>
+        <button className="rounded-md bg-blue-600 p-3 text-white hover:bg-blue-800">
+          Download
+        </button>
+      </nav>
+      <div className="w-full flex gap-5 h-full border">
+        <Editor
+          height="80vh"
+          defaultLangauge="markdown"
+          defaultValue={value}
+          onChange={(e) => setValue(e)}
+          theme="light"
+        />
+        <ReactMarkdown
+          remarkPlugins={[remarkGfm]}
+          rehypePlugins={[rehypeRaw]}
+          className="w-full h-[80vh] border-l-2 border-gray-300 p-5 prose prose-sm overflow-scroll"
+        >
+          {value}
+        </ReactMarkdown>
       </div>
-      <button className='rounded-lg bg-blue-600 p-5 text-white mt-5 hover:bg-blue-800'>Download</button>
+      <footer className="text-center mt-2 text-gray-800 text-sm">
+        @qu1etboy. All right reserved.
+      </footer>
     </div>
   );
 }
