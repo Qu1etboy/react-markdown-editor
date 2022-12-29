5ffcc599ecba618f3910de977aa5d17699b21c66
Qu1etboy <nonzagreanthai@gmail.com>
2022-12-29 16:31:10 +0700

    feat: add download modal

---
+++ components/DownloadModal.jsx
@@ -0,0 +1,9 @@
+const DownloadModal = () => {
+  return (
+    <div className="w-full fixed z-50 top-0 left-0 right-0 bottom-0 bg-blue-500 h-5 p-5 border">
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default DownloadModal;
--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import Editor from "@monaco-editor/react";
 import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
+import DownloadModal from "../components/DownloadModal";
 
 function App() {
   const [value, setValue] = useState(() => {
@@ -15,41 +16,89 @@ function App() {
     }
   });
 
+  const [fileName, setFileName] = useState("");
+  const [openModal, setOpenModal] = useState(false);
+
   useEffect(() => {
     window.localStorage.setItem("value", value);
   }, [value]);
 
-  const handleDownload = () => {
+  const handleDownload = (e) => {
+    e.preventDefault();
+
+    if (fileName === "") {
+      return;
+    }
+
     // create fileUrl from blob
     const blob = new Blob([value]);
     const fileUrl = URL.createObjectURL(blob);
     // create download link
     const link = document.createElement("a");
     // create file name
-    link.download = "markdown.md";
+    link.download = fileName + ".md";
     link.href = fileUrl;
     // click to download
     link.click();
   };
 
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen w-full">
       <nav className="flex justify-between items-center w-full p-5">
-        <h1 className="text-3xl font-bold text-center">Markdown Editor</h1>
+        <h1 className="text-xl md:text-2xl font-bold text-center">
+          Markdown Editor
+        </h1>
         <button
-          className="rounded-md bg-blue-600 p-3 text-white hover:bg-blue-800"
-          onClick={handleDownload}
+          className="rounded-md bg-blue-600 text-sm p-3 text-white hover:bg-blue-800"
+          onClick={() => setOpenModal(true)}
         >
           Download
         </button>
       </nav>
-      <div className="grid gird-cols-1 lg:grid-cols-2 border">
+      {openModal && (
+        <>
+          <div className="w-full h-screen fixed z-20 top-0 left-0 right-0 bottom-0 p-5">
+            <div className="flex flex-col justify-center items-center w-full h-full inset-0">
+              <div className="flex flex-col p-5 rounded-lg bg-white w-[1024px]">
+                <h1 className="text-xl font-bold">Download as Markdown file</h1>
+                <form className="mt-10" onSubmit={(e) => handleDownload(e)}>
+                  <label>File name</label>
+                  <input
+                    placeholder="File name"
+                    className="border rounded-md w-full p-3 mt-3"
+                    onChange={(e) => setFileName(e.target.value)}
+                    required
+                  />
+                  <div className="flex gap-3 float-right mt-10">
+                    <button
+                      type="cancel"
+                      className="hover:bg-red-500 hover:text-white p-3 rounded-md duration-300"
+                      onClick={() => setOpenModal(false)}
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      type="submit"
+                      className="p-3 rounded-md bg-green-500 text-white hover:bg-green-700 duration-300"
+                    >
+                      Save
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+          <div className="fixed inset-0 z-10 opacity-30 bg-black"></div>
+        </>
+      )}
+      <div className="flex flex-col lg:grid grid-cols-2 border">
         <Editor
           height="80vh"
           defaultLangauge="markdown"
           defaultValue={value}
           onChange={(e) => setValue(e)}
           theme="light"
+          className="z-0"
         />
         <ReactMarkdown
           remarkPlugins={[remarkGfm]}
